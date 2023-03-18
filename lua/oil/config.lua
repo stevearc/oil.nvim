c07cf810eb31a48661f2117c243323ef22437c52
nyngwang <nyngwang@gmail.com>
2023-03-19 07:12:18 +0800

    fix Stylua

--- lua/oil/config.lua
+++ lua/oil/config.lua
@@ -59,9 +59,9 @@ local default_config = {
       return vim.startswith(name, ".")
     end,
     -- This function defines what will never be shown, even when `show_hidden` is set
-    is_always_hidden = function (name, bufnr)
+    is_always_hidden = function(name, bufnr)
       return false
-    end
+    end,
   },
   -- Configuration for the floating window in oil.open_float
   float = {
