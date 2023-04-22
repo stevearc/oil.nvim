2bca582d935b723e67a41ec8c2d00684a3d1fc8a
Steven Arcangeli <stevearc@stevearc.com>
2023-04-21 21:57:02 -0700

    feat: extension for resession.nvim

---
+++ lua/resession/extensions/oil.lua
@@ -0,0 +1,23 @@
+local M = {}
+
+M.on_save = function()
+  return {}
+end
+
+M.on_load = function(data) end
+
+M.is_win_supported = function(winid, bufnr)
+  return vim.bo[bufnr].filetype == "oil"
+end
+
+M.save_win = function(winid)
+  local bufnr = vim.api.nvim_win_get_buf(winid)
+  local bufname = vim.api.nvim_buf_get_name(bufnr)
+  return { bufname = bufname }
+end
+
+M.load_win = function(winid, config)
+  require("oil").open(config.bufname)
+end
+
+return M
