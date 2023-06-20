53dcf6ee4910bbec1ca6163e3bfa7e028ac55962
Andrew Cohen <andrewcohen22@gmail.com>
2023-06-20 11:25:30 -0400

    Escape dollar sign

--- lua/oil/util.lua
+++ lua/oil/util.lua
@@ -12,7 +12,7 @@ end
 ---@param filename string
 ---@return string
 M.escape_filename = function(filename)
-  local ret = filename:gsub("([%%#])", "\\%1")
+  local ret = filename:gsub("([%%#$])", "\\%1")
   return ret
 end
 
