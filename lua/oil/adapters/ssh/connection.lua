54dc75986985056ecff31af4c560f8a0292a09fd
Anna Arad <4895022+annagrram@users.noreply.github.com>
2024-07-17 01:58:11 +0300

    fix: Handle users and groups with spaces correctly in SSH

--- lua/oil/adapters/ssh/connection.lua
+++ lua/oil/adapters/ssh/connection.lua
@@ -156,7 +156,7 @@ function SSHConnection.new(url)
   else
     self.jid = jid
   end
-  self:run("whoami", function(err, lines)
+  self:run("id -u", function(err, lines)
     if err then
       vim.notify(string.format("Error fetching ssh connection user: %s", err), vim.log.levels.WARN)
     else
@@ -164,7 +164,7 @@ function SSHConnection.new(url)
       self.meta.user = vim.trim(table.concat(lines, ""))
     end
   end)
-  self:run("groups", function(err, lines)
+  self:run("id -G", function(err, lines)
     if err then
       vim.notify(
         string.format("Error fetching ssh connection user groups: %s", err),
