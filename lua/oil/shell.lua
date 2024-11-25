3c2de37accead0240fbe812f5ccdedfe0b973557
Steven Arcangeli <stevearc@stevearc.com>
2024-11-25 09:10:29 -0800

    debug: include shell command in error message

--- lua/oil/shell.lua
+++ lua/oil/shell.lua
@@ -26,7 +26,8 @@ M.run = function(cmd, opts, callback)
           if err == "" then
             err = "Unknown error"
           end
-          callback(err)
+          local cmd_str = type(cmd) == "string" and cmd or table.concat(cmd, " ")
+          callback(string.format("Error running command '%s'\n%s", cmd_str, err))
         end
       end),
     })
