20baf827476a14d09df1783ca4f0a8d012d5c597
Steven Arcangeli <stevearc@stevearc.com>
2025-02-11 17:42:01 -0800

    ci: update nvim install script for new appimage name

--- .github/workflows/install_nvim.sh
+++ .github/workflows/install_nvim.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 set -e
-PLUGINS="$HOME/.local/share/nvim/site/pack/plugins/start"
-mkdir -p "$PLUGINS"
-
-wget "https://github.com/neovim/neovim/releases/download/${NVIM_TAG-stable}/nvim.appimage"
+version="${NVIM_TAG-stable}"
+dl_name="nvim-linux-x86_64.appimage"
+# The appimage name changed in v0.10.4
+if python -c 'from packaging.version import Version; import sys; sys.exit(not (Version(sys.argv[1]) < Version("v0.10.4")))' "$version" 2>/dev/null; then
+  dl_name="nvim.appimage"
+fi
+curl -sL "https://github.com/neovim/neovim/releases/download/${version}/${dl_name}" -o nvim.appimage
 chmod +x nvim.appimage
 ./nvim.appimage --appimage-extract >/dev/null
 rm -f nvim.appimage
 mkdir -p ~/.local/share/nvim
 mv squashfs-root ~/.local/share/nvim/appimage
 sudo ln -s "$HOME/.local/share/nvim/appimage/AppRun" /usr/bin/nvim
+/usr/bin/nvim --version
