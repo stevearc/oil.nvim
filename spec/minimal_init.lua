36cc369de30fe3f8cd2e3b645bdffc30e9fc715a
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-22 00:32:41 -0500

    fix(ci): set ft plugin indent

--- spec/minimal_init.lua
+++ spec/minimal_init.lua
@@ -2,6 +2,7 @@ vim.cmd([[set runtimepath=$VIMRUNTIME]])
 vim.opt.runtimepath:append('.')
 vim.opt.packpath = {}
 vim.o.swapfile = false
+vim.cmd('filetype plugin indent on')
 vim.fn.mkdir(vim.fn.stdpath('cache'), 'p')
 vim.fn.mkdir(vim.fn.stdpath('data'), 'p')
 vim.fn.mkdir(vim.fn.stdpath('state'), 'p')
